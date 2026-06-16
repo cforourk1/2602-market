927595ba8d022f76fccf5599afae2918ce313897
Crystal <cforourk@umich.edu>
2026-06-15 21:37:20 -0400

    schema done

--- db/schema.sql
+++ db/schema.sql
@@ -1,32 +1,33 @@
+DROP TABLE IF EXISTS orders_products CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+DROP TABLE IF EXISTS products CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+
 CREATE TABLE users (
   id serial PRIMARY KEY,
   username text UNIQUE NOT NULL,
   password text NOT NULL
-)
-
-CREATE TABLE orders (
-  id serial PRIMARY KEY,
-  orderDate date NOT NULL,
-  note text,
-  user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE
-)
-
-CREATE TABLE orders_products (
-  order_id int NOT NULL,
-  product_id int NOT NULL REFERENCES product(id) ON DELETE CASCADE,
-  quantity int NOT NULL
-
-  indexes (
-    (order_id, product_id) PRIMARY KEY
-  )
-)
+);
 
 CREATE TABLE products (
   id serial PRIMARY KEY,
   title text NOT NULL,
   description text NOT NULL,
   price decimal NOT NULL
-)
+);
 
+CREATE TABLE orders (
+  id serial PRIMARY KEY,
+  date date NOT NULL,
+  note text,
+  user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE orders_products (
+  order_id int NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+  product_id int NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+  quantity int NOT NULL,
+  PRIMARY KEY (order_id, product_id)
+);
 
 
