338ccb3910e02f4b2a557b13ded58981be46d41d
Crystal <cforourk@umich.edu>
2026-06-18 20:57:09 -0400

    reusing a lot from jukebox and task

---
+++ api/orders.js
@@ -0,0 +1,49 @@
+import express from "express";
+import { createOrder, getOrderById, getOrders } from "#db/queries/orders";
+import { createOrderProduct } from "#db/queries/orders_products";
+import { getProductsByOrderId, getProductById } from "#db/queries/products";
+import requireBody from "#middleware/requireBody";
+import requireUser from "#middleware/requireUser";
+
+const router = express.Router();
+export default router;
+
+router.use(requireUser);
+
+router.get("/", async (req, res) => {
+  const orders = await getOrders(req.user.id);
+  res.send(orders);
+});
+
+router.post("/", requireBody(["date"]), async (req, res) => {
+  const { date, note } = req.body;
+  const order = await createOrder(date, note, req.user.id);
+  res.status(201).send(order);
+});
+
+router.param("id", async (req, res, next, id) => {
+  const order = await getOrderById(id);
+  if (!order) return res.status(404).send("Order not found.");
+  req.order = order;
+  next();
+});
+
+router.get("/:id", (req, res) => {
+  if (req.order.user_id !== req.user.id) return res.status(403).send("Forbidden.");
+  res.send(req.order);
+});
+
+router.get("/:id/products", async (req, res) => {
+  if (req.order.user_id !== req.user.id) return res.status(403).send("Forbidden.");
+  const products = await getProductsByOrderId(req.order.id);
+  res.send(products);
+});
+
+router.post("/:id/products", requireBody(["productId", "quantity"]), async (req, res) => {
+  if (req.order.user_id !== req.user.id) return res.status(403).send("Forbidden.");
+  const { productId, quantity } = req.body;
+  const product = await getProductById(productId);
+  if (!product) return res.status(400).send("Product not found.");
+  const orderProduct = await createOrderProduct(req.order.id, productId, quantity);
+  res.status(201).send(orderProduct);
+});
--- db/seed.js
+++ db/seed.js
@@ -13,15 +13,18 @@ console.log("🌱 Database seeded.");
 async function seed() {
   const user = await createUser(faker.internet.username(), "password12345");
 
-  for (let i = 0; i < 10; i++) {
-    await createProduct(
-      faker.commerce.productName(),
-      faker.commerce.productDescription(),
-      faker.commerce.price()
-    );
-  }
+await createProduct("Leash and collar set", "Assorted colors for dogs up to 20lbs", 19.99);
+await createProduct("Tennis Ball", "Great for playing fetch", 1.99);
+await createProduct("Lincoln's best dog food", "Roast beef, rice, and yams", 0.99);
+await createProduct("Lincoln's Treat Mix", "Various dried meats", 9.99);
+await createProduct("Dog Bed", "Cozy bed with extra soft fleece", 49.99);
+await createProduct("Dog Blanket", "Soft fleece blanket sized for medium size dogs", 19.99);
+await createProduct("Hot Dog Stuffie", "Perfect for game days!", 8.99);
+await createProduct("Rope Toy", "Small braided rope", 6.99);
+await createProduct("Squeaky Ball", "Small extra loud squeaky ball for maximum chaos", 5.99);
+await createProduct("Waste Bags", "100 count biodegradable waste bags", 9.99);
 
-  const order = await createOrder(faker.date.past(), faker.lorem.sentence(), user.id);
+const order = await createOrder(faker.date.past(), "Please leave at door, Lincoln will bark.", user.id);
 
   for (let i = 1; i <= 5; i++) {
     await createOrderProduct(order.id, i, faker.number.int({ min: 1, max: 10 }));
